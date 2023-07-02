3e2b52a454ec313b7d2ac0da3117ad4316a6c464
Igor Dias <igorverissimo02@hotmail.com>
2023-07-02 13:55:26 -0300

    realizado a divisão do código em dois arquivos

---
+++ editor_imagem.py
@@ -0,0 +1,129 @@
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import numpy as np
+
+
+class EditImage():
+    def __init__(self, janela,label_imagem):
+        self.imagem = None
+        self.start_x = None
+        self.start_y = None
+        self.end_x = None
+        self.end_y = None
+        self.is_selecting = False
+        self.regiao_selecionada =False
+        self.imagem_original = None
+        self.imagem_anterior = None
+        self.label_imagem = label_imagem
+        self.janela = janela
+       
+    def abrir_imagem(self):
+  
+
+        self.file_path = filedialog.askopenfilename()
+        self.imagem = cv2.imread(self.file_path)
+        self.imagem_original = self.imagem.copy()
+        self.imagem_anterior = self.imagem.copy()
+        imagem_rgb = cv2.cvtColor(self.imagem, cv2.COLOR_BGR2RGB)
+        imagem_tk = ImageTk.PhotoImage(Image.fromarray(imagem_rgb))
+        self.label_imagem.configure(image=imagem_tk)
+        self.label_imagem.image = imagem_tk
+
+
+    def aplicar_filtro(self, filtro):      
+
+        mask = np.array(filtro).reshape(3, 3)
+        self.imagem_anterior = self.imagem.copy()
+        if self.regiao_selecionada:
+            regiao_selecionada = np.zeros_like(self.imagem[:, :, 0])
+            regiao_selecionada[self.start_y:self.end_y , self.start_x: self.end_x] = 255
+            imagem_aux =  self.imagem[self.start_y:self.end_y, self.start_x:self.end_x]
+            frameFiltered = cv2.filter2D(imagem_aux, -1, mask, anchor=(1, 1), delta=0, borderType=cv2.BORDER_DEFAULT)
+            imagem_aux = self.imagem.copy()
+            for x in range(self.start_x, self.end_x):
+                for y in range(self.start_y, self.end_y):
+                    imagem_aux[y][x] = frameFiltered[y-self.start_y][x-self.start_x]
+            frameFiltered = imagem_aux.copy()
+            self.regiao_selecionada = False
+       
+        else:
+            frameFiltered = cv2.filter2D(self.imagem.copy(), -1, mask, anchor=(1, 1), delta=0, borderType=cv2.BORDER_DEFAULT)
+
+        result = np.uint8(frameFiltered)
+        self.imagem = result.copy()
+        result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+        imagem_tk = ImageTk.PhotoImage(Image.fromarray(result))
+        
+        self.label_imagem.configure(image=imagem_tk)
+        
+        self.label_imagem.image = imagem_tk
+    
+    def media(self):
+        media = [0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111]
+        self.aplicar_filtro(media)
+    def gauss(self):
+        gauss = [0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625]
+        self.aplicar_filtro(gauss)
+    def horizontal(self):
+        horizontal = [-1, 0, 1, -2, 0, 2, -1, 0, 1]
+        self.aplicar_filtro(horizontal)
+    def vertical(self):
+        vertical = [-1, -2, -1, 0, 0, 0, 1, 2, 1]
+        self.aplicar_filtro(vertical)
+    def laplacian(self):
+        laplacian = [0, -1, 0, -1, 4, -1, 0, -1, 0]
+        self.aplicar_filtro(laplacian)
+    def boost(self):
+        boost = [0, -1, 0, -1, 5.2, -1, 0, -1, 0]
+        self.aplicar_filtro(boost)    
+   
+    def on_mouse_move(self, event):
+        if self.file_path and self.is_selecting and event.widget == self.label_imagem:
+            
+            self.end_x = event.x
+            self.end_y = event.y
+            imagem_com_retangulo = self.imagem.copy()
+            cv2.rectangle(imagem_com_retangulo, (self.start_x, self.start_y), (self.end_x, self.end_y), (255, 0, 0), 2)
+            
+            imagem_com_retangulo_rgb = cv2.cvtColor(imagem_com_retangulo, cv2.COLOR_BGR2RGB)
+            imagem_tk = ImageTk.PhotoImage(Image.fromarray(imagem_com_retangulo_rgb))
+            self.label_imagem.configure(image=imagem_tk)
+            self.label_imagem.image = imagem_tk
+   
+    def on_mouse_down(self, event):
+        if self.file_path and event.widget == self.label_imagem:
+            self.start_x = event.x
+            self.start_y = event.y
+            self.is_selecting = True
+    def on_mouse_up(self, event):
+        if self.file_path and self.is_selecting:
+            self.is_selecting = False
+            # imagem = cv2.imread(self.file_path)
+            imagem_com_retangulo = self.imagem.copy()
+            cv2.rectangle(imagem_com_retangulo, (self.start_x, self.start_y), (self.end_x, self.end_y), (255, 0, 0), 2)
+            imagem_com_retangulo_rgb = cv2.cvtColor(imagem_com_retangulo, cv2.COLOR_BGR2RGB)
+            imagem_tk = ImageTk.PhotoImage(Image.fromarray(imagem_com_retangulo_rgb))
+            self.label_imagem.configure(image=imagem_tk)
+            self.label_imagem.image = imagem_tk
+            self.regiao_selecionada = True 
+
+    def restaurar_imagem(self):
+        self.imagem = self.imagem_original.copy()
+        resultado = cv2.cvtColor(self.imagem, cv2.COLOR_BGR2RGB)
+        imagem_tk = ImageTk.PhotoImage(Image.fromarray( resultado))
+        self.label_imagem.configure(image=imagem_tk)
+        self.label_imagem.image = imagem_tk
+        
+    def desfazer_alteracao(self):
+        self.imagem = self.imagem_anterior.copy()
+        resultado = cv2.cvtColor(self.imagem, cv2.COLOR_BGR2RGB)
+        imagem_tk = ImageTk.PhotoImage(Image.fromarray( resultado))
+        self.label_imagem.configure(image=imagem_tk)
+        self.label_imagem.image = imagem_tk
+
+    def iniciar_selecao(self):
+        self.janela.bind("<Button-1>", self.on_mouse_down)
+        self.janela.bind("<B1-Motion>", self.on_mouse_move)
+        self.janela.bind("<ButtonRelease-1>", self.on_mouse_up)
--- main.py
+++ main.py
@@ -3,127 +3,8 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import ImageTk, Image
 import numpy as np
+from editor_imagem import EditImage
 
-
-class EditImage():
-    def __init__(self):
-        self.imagem = None
-        self.start_x = None
-        self.start_y = None
-        self.end_x = None
-        self.end_y = None
-        self.is_selecting = False
-        self.regiao_selecionada =False
-        self.imagem_original = None
-        self.imagem_anterior = None
-       
-    def abrir_imagem(self):
-  
-
-        self.file_path = filedialog.askopenfilename()
-        self.imagem = cv2.imread(self.file_path)
-        self.imagem_original = self.imagem.copy()
-        self.imagem_anterior = self.imagem.copy()
-        imagem_rgb = cv2.cvtColor(self.imagem, cv2.COLOR_BGR2RGB)
-        imagem_tk = ImageTk.PhotoImage(Image.fromarray(imagem_rgb))
-        label_imagem.configure(image=imagem_tk)
-        label_imagem.image = imagem_tk
-
-
-    def aplicar_filtro(self, filtro):      
-
-        mask = np.array(filtro).reshape(3, 3)
-        self.imagem_anterior = self.imagem.copy()
-        if self.regiao_selecionada:
-            regiao_selecionada = np.zeros_like(self.imagem[:, :, 0])
-            regiao_selecionada[self.start_y:self.end_y , self.start_x: self.end_x] = 255
-            imagem_aux =  self.imagem[self.start_y:self.end_y, self.start_x:self.end_x]
-            frameFiltered = cv2.filter2D(imagem_aux, -1, mask, anchor=(1, 1), delta=0, borderType=cv2.BORDER_DEFAULT)
-            imagem_aux = self.imagem.copy()
-            for x in range(self.start_x, self.end_x):
-                for y in range(self.start_y, self.end_y):
-                    imagem_aux[y][x] = frameFiltered[y-self.start_y][x-self.start_x]
-            frameFiltered = imagem_aux.copy()
-            self.regiao_selecionada = False
-       
-        else:
-            frameFiltered = cv2.filter2D(self.imagem.copy(), -1, mask, anchor=(1, 1), delta=0, borderType=cv2.BORDER_DEFAULT)
-
-        result = np.uint8(frameFiltered)
-        self.imagem = result.copy()
-        result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
-        imagem_tk = ImageTk.PhotoImage(Image.fromarray(result))
-        
-        label_imagem.configure(image=imagem_tk)
-        
-        label_imagem.image = imagem_tk
-    
-    def media(self):
-        media = [0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111]
-        self.aplicar_filtro(media)
-    def gauss(self):
-        gauss = [0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625]
-        self.aplicar_filtro(gauss)
-    def horizontal(self):
-        horizontal = [-1, 0, 1, -2, 0, 2, -1, 0, 1]
-        self.aplicar_filtro(horizontal)
-    def vertical(self):
-        vertical = [-1, -2, -1, 0, 0, 0, 1, 2, 1]
-        self.aplicar_filtro(vertical)
-    def laplacian(self):
-        laplacian = [0, -1, 0, -1, 4, -1, 0, -1, 0]
-        self.aplicar_filtro(laplacian)
-    def boost(self):
-        boost = [0, -1, 0, -1, 5.2, -1, 0, -1, 0]
-        self.aplicar_filtro(boost)    
-   
-    def on_mouse_move(self, event):
-        if self.file_path and self.is_selecting and event.widget == label_imagem:
-            
-            self.end_x = event.x
-            self.end_y = event.y
-            imagem_com_retangulo = self.imagem.copy()
-            cv2.rectangle(imagem_com_retangulo, (self.start_x, self.start_y), (self.end_x, self.end_y), (255, 0, 0), 2)
-            
-            imagem_com_retangulo_rgb = cv2.cvtColor(imagem_com_retangulo, cv2.COLOR_BGR2RGB)
-            imagem_tk = ImageTk.PhotoImage(Image.fromarray(imagem_com_retangulo_rgb))
-            label_imagem.configure(image=imagem_tk)
-            label_imagem.image = imagem_tk
-   
-    def on_mouse_down(self, event):
-        if self.file_path and event.widget == label_imagem:
-            self.start_x = event.x
-            self.start_y = event.y
-            self.is_selecting = True
-    def on_mouse_up(self, event):
-        if self.file_path and self.is_selecting:
-            self.is_selecting = False
-            # imagem = cv2.imread(self.file_path)
-            imagem_com_retangulo = self.imagem.copy()
-            cv2.rectangle(imagem_com_retangulo, (self.start_x, self.start_y), (self.end_x, self.end_y), (255, 0, 0), 2)
-            imagem_com_retangulo_rgb = cv2.cvtColor(imagem_com_retangulo, cv2.COLOR_BGR2RGB)
-            imagem_tk = ImageTk.PhotoImage(Image.fromarray(imagem_com_retangulo_rgb))
-            label_imagem.configure(image=imagem_tk)
-            label_imagem.image = imagem_tk
-            self.regiao_selecionada = True 
-
-    def restaurar_imagem(self):
-        self.imagem = self.imagem_original.copy()
-        resultado = cv2.cvtColor(self.imagem, cv2.COLOR_BGR2RGB)
-        imagem_tk = ImageTk.PhotoImage(Image.fromarray( resultado))
-        label_imagem.configure(image=imagem_tk)
-        label_imagem.image = imagem_tk
-    def desfazer_alteracao(self):
-        self.imagem = self.imagem_anterior.copy()
-        resultado = cv2.cvtColor(self.imagem, cv2.COLOR_BGR2RGB)
-        imagem_tk = ImageTk.PhotoImage(Image.fromarray( resultado))
-        label_imagem.configure(image=imagem_tk)
-        label_imagem.image = imagem_tk
-
-    def iniciar_selecao(self):
-        janela.bind("<Button-1>", self.on_mouse_down)
-        janela.bind("<B1-Motion>", self.on_mouse_move)
-        janela.bind("<ButtonRelease-1>", self.on_mouse_up)
 
 
 janela = tk.Tk()
@@ -132,7 +13,10 @@ screen_height = janela.winfo_screenheight()
 janela.geometry(f"{screen_width}x{screen_height}")
 frame_botoes = tk.Frame(janela)
 frame_botoes.pack()
-edit_image = EditImage()
+frame_imagem = tk.Frame(janela)
+label_imagem = tk.Label(frame_imagem)
+
+edit_image = EditImage(janela, label_imagem)
 botao_abrir_imagem = tk.Button(frame_botoes, text="Abrir Imagem", command=edit_image.abrir_imagem)
 botao_media = tk.Button(frame_botoes, text="Filtro Média", command=edit_image.media)
 botao_gaus = tk.Button(frame_botoes, text="Filtro Gauss", command=edit_image.gauss)
@@ -155,9 +39,9 @@ botao_selecao.grid(row=0, column=7)
 botao_restaurar.grid(row=0, column=8)
 botao_desfazer.grid(row=0, column=9)
 
-frame_imagem = tk.Frame(janela)
+
 frame_imagem.pack()
-label_imagem = tk.Label(frame_imagem)
+
 label_imagem.pack()
 
 janela.mainloop()
